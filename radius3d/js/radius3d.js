38982993f430c96b174430c08e937aba59b94363
Hang Le <hang.le@radius.com>
2016-05-12 14:16:40 -0700

    current with text

--- radius3d/js/radius3d.js
+++ radius3d/js/radius3d.js
@@ -5,7 +5,9 @@ var rotate = false;
 var camera, controls, scene, renderer, raycaster,target;
 var mouse = new THREE.Vector2(), INTERSECTED;
 var realMouseX, realMouseY;
-
+var jsonurl = "data/insightsresponse.json";
+var font;
+var intersectables = [];
 function init() {
 
 	scene = new THREE.Scene();
@@ -37,37 +39,8 @@ function init() {
 	controls.enableDamping = true;
 	controls.dampingFactor = 0.25;
 	controls.enableZoom = false;*/
-
-	// world
-
-	// Geometry definition
-	var geometry = new THREE.IcosahedronGeometry(1,0);
-	//var geometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
-	//var material =  new THREE.MeshPhongMaterial( { color:0xffffff, shading: THREE.FlatShading } );
-
-	//Creating Objects
-	for ( var i = 0; i < 500; i ++ ) {
-		//var mesh = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff }) );
-		var randomRadius = (Math.random() - 0.2) * 10;
-		var mesh = new THREE.Mesh( new THREE.IcosahedronGeometry(randomRadius,1), 
-			new THREE.MeshPhongMaterial( {
-				color: 0x156289,
-				emissive: 0x072534,
-				side: THREE.DoubleSide,
-				shading: THREE.FlatShading
-			} )
-		);
-		
-		mesh.position.x = ( Math.random() - 0.5 ) * 500;
-		mesh.position.y = ( Math.random() - 0.5 ) * 500;
-		mesh.position.z = ( Math.random() - 0.5 ) * 500;
-		mesh.updateMatrix();
-		mesh.matrixAutoUpdate = false;
-		scene.add( mesh );
-	}
-
+	
 	// lights
-
 	light = new THREE.DirectionalLight( 0xA7A7A7 );
 	light.position.set( 1, 1, 1 );
 	scene.add( light );
@@ -85,12 +58,95 @@ function init() {
 	stats = new Stats();
 	container.appendChild(stats.dom);
 
-	//
-
 	window.addEventListener( 'resize', onWindowResize, false );
 	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	
+	//Load font & Generate Json for Objects on scene
+	var loader = new THREE.FontLoader();
+	loader.load( 'fonts/' + 'helvetiker_regular.typeface.js', function ( response ) {
+		font = response;
+		getJson();
+	});
+}
+
+// GENERATING BLOBS BASED ON JSON
+function generateBlobs(returnJson){
+	console.log(returnJson);
+	console.log("Generating blobs");
+	// Geometry definition
+	var geometry = new THREE.IcosahedronGeometry(1,0);
+	//Creating Groups
+	for (var insightTitle in returnJson.payload){
+		//Creating text field for Title
+		console.log("Creating title" + insightTitle);
+		var textMesh = generateText(insightTitle);
+		scene.add(textMesh);
+	}
+
+	//Creating Objects in Group
+	for ( var i = 0; i < 500; i ++ ) {
+		//var mesh = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff }) );
+		var randomRadius = (Math.random() - 0.2) * 10;
+		var mesh = new THREE.Mesh( new THREE.IcosahedronGeometry(randomRadius,1), 
+			new THREE.MeshPhongMaterial( {
+				color: 0x156289,
+				emissive: 0x072534,
+				side: THREE.DoubleSide,
+				shading: THREE.FlatShading
+			} )
+		);
+		
+		mesh.position.x = ( Math.random() - 0.5 ) * 500;
+		mesh.position.y = ( Math.random() - 0.5 ) * 500;
+		mesh.position.z = ( Math.random() - 0.5 ) * 500;
+		mesh.updateMatrix();
+		mesh.matrixAutoUpdate = false;
+		scene.add(mesh);
+		scene.add()
+		intersectables.push(mesh);
+	}
 
 }
+function getJson(){
+	console.log("Getting Json");
+	//Getting Json file (local)
+	var request = $.ajax({
+					url: jsonurl,
+					crossDomain: true,
+				});
+	request.done(function(returnJson){
+		generateBlobs(returnJson);
+	}) //end request done
+}
+
+// GENERATING 3D TEXT
+function generateText(textValue){
+	textGeo = new THREE.TextGeometry( textValue, {
+					font: font,
+					size: 30,
+					height: 20,
+					curveSegments: 4,
+					bevelThickness: 1,
+					bevelSize: 1.5,
+					bevelEnabled: true,
+					material: 0,
+					extrudeMaterial: 1
+				});
+	var material = new THREE.MultiMaterial( [
+					new THREE.MeshPhongMaterial( { color: 0xffffff, shading: THREE.FlatShading } ), // front
+					new THREE.MeshPhongMaterial( { color: 0xffffff, shading: THREE.SmoothShading } ) // side
+				] );
+	textGeo.computeBoundingBox();
+	textGeo.computeVertexNormals();
+	var textMesh1 = new THREE.Mesh( textGeo, material );
+	textMesh1.position.x = ( Math.random() - 0.5 ) * 500;
+	textMesh1.position.y = ( Math.random() - 0.5 ) * 500;
+	textMesh1.position.z = ( Math.random() - 0.5 ) * 500;
+	textMesh1.rotation.x = 0;
+	textMesh1.rotation.y = Math.PI * 2;
+	return textMesh1;
+}
+
 
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
@@ -98,14 +154,10 @@ function onWindowResize() {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function animate() {
-
+function animate(){
 	requestAnimationFrame( animate );
-
 	controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
-
 	stats.update();
-
 	render();
 
 }
@@ -132,7 +184,7 @@ function render() {
 	//INTERACTIVE
 	// find intersections
 	raycaster.setFromCamera(mouse,camera );
-	var intersects = raycaster.intersectObjects( scene.children );
+	var intersects = raycaster.intersectObjects(intersectables);
 	if ( intersects.length > 0 ) {
 		if ( INTERSECTED != intersects[ 0 ].object ) {
 			if (INTERSECTED) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
@@ -152,7 +204,6 @@ function render() {
 /* INTERACTION ON TOP OF SCENE */
 function displayHover(intersectedObject){
 		//Place hover at the position
-		console.log("Hovering ");
 		$(".hoverPanel").css({left: realMouseX + "px", top: realMouseY + "px"});
 }
 function addSignal(){
