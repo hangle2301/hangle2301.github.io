5fdda7d87b4d093d17edbea276fa2193f31bdcb1
Hang Le <hangle2301@gmail.com>
2016-11-30 12:52:17 -0800

    Changing places

---
+++ contacts/Round-2/Builder1-T.framer/imported/Contacts-Prototype-Opt3-T@1x/layers.json.js
@@ -0,0 +1,571 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["Contacts-Prototype-Opt3-T@1x/layers.json.js"] = [
+	{
+		"objectId": "ABFC2DB4-BA71-49AF-B5F4-D942FFBC7832",
+		"kind": "artboard",
+		"name": "Opt3",
+		"originalName": "Opt3",
+		"maskFrame": null,
+		"layerFrame": {
+			"x": 2612,
+			"y": -135,
+			"width": 799,
+			"height": 407
+		},
+		"visible": true,
+		"metadata": {},
+		"backgroundColor": "rgba(255, 255, 255, 1)",
+		"children": [
+			{
+				"objectId": "F8936868-6D00-4AB4-8265-E48ED0F98F22",
+				"kind": "text",
+				"name": "Right",
+				"originalName": "Right",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 66,
+					"y": 79,
+					"width": 191,
+					"height": 69
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1,
+					"string": "Selected Attributes:\n\nCompanies: IT Companies\nContacts: VP or Director",
+					"css": [
+						"/* Selected Attributes:: */",
+						"opacity: 0.89;",
+						"font-family: ApexNew-Medium;",
+						"font-size: 18px;",
+						"color: #303B3E;",
+						"letter-spacing: 0;",
+						"line-height: 18px;"
+					]
+				},
+				"image": {
+					"path": "images/Layer-Right-rjg5mzy4.png",
+					"frame": {
+						"x": 66,
+						"y": 79,
+						"width": 191,
+						"height": 69
+					}
+				},
+				"children": []
+			},
+			{
+				"objectId": "31DE3156-2234-474E-9F73-44064C38F169",
+				"kind": "group",
+				"name": "everything",
+				"originalName": "everything",
+				"maskFrame": null,
+				"layerFrame": {
+					"x": 322,
+					"y": 78,
+					"width": 314,
+					"height": 246
+				},
+				"visible": true,
+				"metadata": {
+					"opacity": 1
+				},
+				"children": [
+					{
+						"objectId": "4E1B7E38-E6BA-4301-9A62-BE9F3109DFA9",
+						"kind": "group",
+						"name": "nextBt",
+						"originalName": "nextBt",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 422,
+							"y": 302,
+							"width": 115,
+							"height": 22
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"image": {
+							"path": "images/Layer-nextBt-neuxqjdf.png",
+							"frame": {
+								"x": 422,
+								"y": 302,
+								"width": 115,
+								"height": 22
+							}
+						},
+						"children": []
+					},
+					{
+						"objectId": "27D4467C-0D2B-43FD-B96D-E8C7B153D485",
+						"kind": "group",
+						"name": "option",
+						"originalName": "option",
+						"maskFrame": null,
+						"layerFrame": {
+							"x": 322,
+							"y": 78,
+							"width": 314,
+							"height": 160
+						},
+						"visible": true,
+						"metadata": {
+							"opacity": 1
+						},
+						"children": [
+							{
+								"objectId": "CF8C2020-C3E9-43F4-8998-9EC163300DE6",
+								"kind": "group",
+								"name": "switcher",
+								"originalName": "switcher",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 442,
+									"y": 95,
+									"width": 94,
+									"height": 70
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"children": [
+									{
+										"objectId": "4CAD240E-2C99-4E7A-9712-CBA55F071A07",
+										"kind": "group",
+										"name": "company1",
+										"originalName": "company",
+										"maskFrame": null,
+										"layerFrame": {
+											"x": 457,
+											"y": 109,
+											"width": 56,
+											"height": 13
+										},
+										"visible": true,
+										"metadata": {
+											"opacity": 1
+										},
+										"children": [
+											{
+												"objectId": "9AE410BD-A978-4AFA-87E0-3E7BCBFE30E5",
+												"kind": "text",
+												"name": "selected",
+												"originalName": "selected",
+												"maskFrame": null,
+												"layerFrame": {
+													"x": 457,
+													"y": 109,
+													"width": 56,
+													"height": 13
+												},
+												"visible": true,
+												"metadata": {
+													"opacity": 1,
+													"string": "Companies",
+													"css": [
+														"/* Companies: */",
+														"opacity: 0.89;",
+														"font-family: ApexNew-Medium;",
+														"font-size: 12px;",
+														"color: #303B3E;",
+														"letter-spacing: 0;",
+														"line-height: 24px;"
+													]
+												},
+												"image": {
+													"path": "images/Layer-selected-ouffndew.png",
+													"frame": {
+														"x": 457,
+														"y": 109,
+														"width": 56,
+														"height": 13
+													}
+												},
+												"children": []
+											},
+											{
+												"objectId": "A4F30E0A-E6C0-4A91-B273-C655627D8E3E",
+												"kind": "text",
+												"name": "not",
+												"originalName": "not",
+												"maskFrame": null,
+												"layerFrame": {
+													"x": 457,
+													"y": 109,
+													"width": 56,
+													"height": 13
+												},
+												"visible": true,
+												"metadata": {
+													"opacity": 1,
+													"string": "Companies",
+													"css": [
+														"/* Companies: */",
+														"opacity: 0.89;",
+														"font-family: ApexNew-Book;",
+														"font-size: 12px;",
+														"color: #303B3E;",
+														"letter-spacing: 0;",
+														"line-height: 24px;"
+													]
+												},
+												"image": {
+													"path": "images/Layer-not-qtrgmzbf.png",
+													"frame": {
+														"x": 457,
+														"y": 109,
+														"width": 56,
+														"height": 13
+													}
+												},
+												"children": []
+											}
+										]
+									},
+									{
+										"objectId": "49154E99-95F1-417B-BF76-6FC43782C108",
+										"kind": "group",
+										"name": "location1",
+										"originalName": "location",
+										"maskFrame": null,
+										"layerFrame": {
+											"x": 459,
+											"y": 134,
+											"width": 50,
+											"height": 10
+										},
+										"visible": true,
+										"metadata": {
+											"opacity": 1
+										},
+										"children": [
+											{
+												"objectId": "FBD0043D-9A29-4A8B-8F91-78C44F2A688D",
+												"kind": "text",
+												"name": "selected1",
+												"originalName": "selected",
+												"maskFrame": null,
+												"layerFrame": {
+													"x": 459,
+													"y": 134,
+													"width": 50,
+													"height": 10
+												},
+												"visible": true,
+												"metadata": {
+													"opacity": 1,
+													"string": "Locations",
+													"css": [
+														"/* Locations: */",
+														"font-family: ApexNew-Medium;",
+														"font-size: 12px;",
+														"color: #303B3E;",
+														"letter-spacing: 0;"
+													]
+												},
+												"image": {
+													"path": "images/Layer-selected-rkjemda0.png",
+													"frame": {
+														"x": 459,
+														"y": 134,
+														"width": 50,
+														"height": 10
+													}
+												},
+												"children": []
+											},
+											{
+												"objectId": "177EFA20-E406-4E67-A189-9873FFBCB96C",
+												"kind": "text",
+												"name": "not1",
+												"originalName": "not",
+												"maskFrame": null,
+												"layerFrame": {
+													"x": 459,
+													"y": 134,
+													"width": 49,
+													"height": 10
+												},
+												"visible": true,
+												"metadata": {
+													"opacity": 1,
+													"string": "Locations",
+													"css": [
+														"/* Locations: */",
+														"font-family: ApexNew-Book;",
+														"font-size: 12px;",
+														"color: #303B3E;",
+														"letter-spacing: 0;"
+													]
+												},
+												"image": {
+													"path": "images/Layer-not-mtc3ruzb.png",
+													"frame": {
+														"x": 459,
+														"y": 134,
+														"width": 49,
+														"height": 10
+													}
+												},
+												"children": []
+											}
+										]
+									},
+									{
+										"objectId": "63812FB8-1B2D-4A55-BBC2-728C1DDD6C33",
+										"kind": "group",
+										"name": "bg1",
+										"originalName": "bg",
+										"maskFrame": null,
+										"layerFrame": {
+											"x": 442,
+											"y": 95,
+											"width": 94,
+											"height": 70
+										},
+										"visible": true,
+										"metadata": {
+											"opacity": 1
+										},
+										"image": {
+											"path": "images/Layer-bg-njm4mtjg.png",
+											"frame": {
+												"x": 442,
+												"y": 95,
+												"width": 94,
+												"height": 70
+											}
+										},
+										"children": []
+									}
+								]
+							},
+							{
+								"objectId": "BE567BFF-28F7-4143-A8D9-3515750DEBE4",
+								"kind": "group",
+								"name": "company",
+								"originalName": "company",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 463,
+									"y": 78,
+									"width": 63,
+									"height": 155
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"children": [
+									{
+										"objectId": "1DB4B2DB-E682-4004-91B9-E7A9D1607FBD",
+										"kind": "text",
+										"name": "title",
+										"originalName": "title",
+										"maskFrame": null,
+										"layerFrame": {
+											"x": 466,
+											"y": 78,
+											"width": 57,
+											"height": 13
+										},
+										"visible": true,
+										"metadata": {
+											"opacity": 1,
+											"string": "Companies",
+											"css": [
+												"/* Companies: */",
+												"opacity: 0.89;",
+												"font-family: ApexNew-Medium;",
+												"font-size: 12px;",
+												"color: #2B81C8;",
+												"letter-spacing: 0;",
+												"line-height: 12px;"
+											]
+										},
+										"image": {
+											"path": "images/Layer-title-murcneiy.png",
+											"frame": {
+												"x": 466,
+												"y": 78,
+												"width": 57,
+												"height": 13
+											}
+										},
+										"children": []
+									},
+									{
+										"objectId": "A251C4E0-4097-407B-965F-9B1B08E54A71",
+										"kind": "group",
+										"name": "Group_6",
+										"originalName": "Group 6",
+										"maskFrame": null,
+										"layerFrame": {
+											"x": 463,
+											"y": 96,
+											"width": 63,
+											"height": 137
+										},
+										"visible": true,
+										"metadata": {
+											"opacity": 1
+										},
+										"image": {
+											"path": "images/Layer-Group_6-qti1mum0.png",
+											"frame": {
+												"x": 463,
+												"y": 96,
+												"width": 63,
+												"height": 137
+											}
+										},
+										"children": []
+									}
+								]
+							},
+							{
+								"objectId": "FE60839B-5414-40FF-8813-C02F1988CC98",
+								"kind": "group",
+								"name": "location",
+								"originalName": "location",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 468,
+									"y": 79,
+									"width": 63,
+									"height": 154
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"children": [
+									{
+										"objectId": "275BA83F-95AB-4A7A-B50E-0F519335F157",
+										"kind": "text",
+										"name": "title1",
+										"originalName": "title",
+										"maskFrame": null,
+										"layerFrame": {
+											"x": 473,
+											"y": 79,
+											"width": 50,
+											"height": 10
+										},
+										"visible": true,
+										"metadata": {
+											"opacity": 1,
+											"string": "Locations",
+											"css": [
+												"/* Locations: */",
+												"opacity: 0.89;",
+												"font-family: ApexNew-Medium;",
+												"font-size: 12px;",
+												"color: #2B81C8;",
+												"letter-spacing: 0;",
+												"line-height: 12px;"
+											]
+										},
+										"image": {
+											"path": "images/Layer-title-mjc1qke4.png",
+											"frame": {
+												"x": 473,
+												"y": 79,
+												"width": 50,
+												"height": 10
+											}
+										},
+										"children": []
+									},
+									{
+										"objectId": "49F4BAAB-A6DB-44E8-ABA6-31764A793015",
+										"kind": "group",
+										"name": "Group_7",
+										"originalName": "Group 7",
+										"maskFrame": null,
+										"layerFrame": {
+											"x": 468,
+											"y": 96,
+											"width": 63,
+											"height": 137
+										},
+										"visible": true,
+										"metadata": {
+											"opacity": 1
+										},
+										"image": {
+											"path": "images/Layer-Group_7-ndlgnejb.png",
+											"frame": {
+												"x": 468,
+												"y": 96,
+												"width": 63,
+												"height": 137
+											}
+										},
+										"children": []
+									}
+								]
+							},
+							{
+								"objectId": "67EB7151-5F62-4325-B016-E45B11732F6D",
+								"kind": "group",
+								"name": "contact",
+								"originalName": "contact",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 538,
+									"y": 78,
+									"width": 98,
+									"height": 155
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-contact-njdfqjcx.png",
+									"frame": {
+										"x": 538,
+										"y": 78,
+										"width": 98,
+										"height": 155
+									}
+								},
+								"children": []
+							},
+							{
+								"objectId": "F78BF57C-6BF5-47F1-9FFB-F93EC625B6F6",
+								"kind": "group",
+								"name": "bg",
+								"originalName": "bg",
+								"maskFrame": null,
+								"layerFrame": {
+									"x": 322,
+									"y": 78,
+									"width": 314,
+									"height": 160
+								},
+								"visible": true,
+								"metadata": {
+									"opacity": 1
+								},
+								"image": {
+									"path": "images/Layer-bg-rjc4qky1.png",
+									"frame": {
+										"x": 322,
+										"y": 78,
+										"width": 314,
+										"height": 160
+									}
+								},
+								"children": []
+							}
+						]
+					}
+				]
+			}
+		]
+	}
+]
